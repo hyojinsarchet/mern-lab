7acbd658451a623674a9b4563040a00775ebad26
hyojin.sarchet <hyojin5931@hanmail.net>
2018-03-16 17:04:32 -0400

    added some components and downloaded axios

---
+++ react-lab/src/ImgContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+
+// Store img data here
+class ImgContainer extends Component {
+    constructor() {
+      super()
+      this.state = {
+
+      }
+      render() {
+        return (
+
+        )
+      }
+      // componentDidMount () {
+      //
+      //       axios.get('http://localhost:3000/gifs')
+      //
+      //   }
+    }
+  }
+
+
+export default ImgContainer
